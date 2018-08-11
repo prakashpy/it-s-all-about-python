7fd48fc4ade4a5ddc47d89e76bbaa3178d02ed7e
Prakash Patel <prakash.py@outlook.com>
2018-08-11 13:02:41 -0500

    os updated, new module added

---
+++ 0-miscellaneous.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+for i in range(10):
+    sleep(5)
+    print i
+
+
--- os.py
+++ os.py
@@ -2,18 +2,19 @@ import os
 import sys
 
 #change present working directory
-os.chdir("/home/analysis")
+if os.path.isdir('/home/user'):
+    os.chdir("/home/user")
 
 # prints present working dir
 print os.getcwd()
 
 # get list of files in some directory
-list_of_files = os.listdir("/home/analysis")
+list_of_files = os.listdir("/home/user")
 print "list_of_files: ", list_of_files
 
 
 # get's the file name and removes all prefixes
-input_file = os.path.basename("/home/analysis/my-test-file.py")
+input_file = os.path.basename("/home/user/my-test-file.py")
 print input_file
 
 PIPELINE_DIR = "/usr/lib/projct_path/modules"
@@ -26,12 +27,14 @@ if os.path.isfile("my-file-name.py") is True:
     print "file exits!"
 
 if os.path.isdir(PIPELINE_DIR) is False:
+    print "something something"
 
-
-
-if str(os.getcwd()) != str(genotype_pipeline_path):
+my_project_path = '/home/user/my_first_project'
+if str(os.getcwd()) != str(my_project_path):
     print "not equal"
 
+file_name = '/home/user/file.txt'
+file_name = 'file.txt'
 #delete file only if exists
 if os.path.isfile(file_name):
     os.remove(file_name)

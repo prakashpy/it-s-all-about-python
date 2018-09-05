54cf0cf1fde47628b9eb6917c687dc9c93f34916
Prakash Patel <prakash.py@outlook.com>
2018-09-05 18:52:06 +0000

    re replacement with endswith & startswith

--- use_of_re_module.py
+++ use_of_re_module.py
@@ -14,3 +14,19 @@ print new_input_renamed
 new_input_renamed = re.sub(r"\.gz$", "", input)
 print new_input_renamed
 # output on console: 12_34_abc.1aX1b_final.vcf
+
+
+
+# TIP 
+""" Replace "re.search('\.gz$', file)" with "file.endswith('.gz')" which is ten-times faster and reduces overhead. """
+file = "some_random_name.vcf.gz"
+if re.search('\.gz$', file):
+    print "File do has gz extension"
+# code below is more efficient than above
+if file.endswith('.gz'):
+    print "File do has gz extension"
+
+if file.startswith('some'):
+    print "file name starts with some."
+
+# root, ext = os.path.splitext(filename)

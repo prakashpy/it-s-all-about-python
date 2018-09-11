089ff27b92ebb496f9cc20735433ae3f81b414c4
Prakash Patel <prakash.py@outlook.com>
2018-09-11 05:17:05 +0000

    f-strings for python3

--- string_formatting.py
+++ string_formatting.py
@@ -1,5 +1,27 @@
 job_id = 10
 
+# str.format()
 project_path = '/tmp/jobs/{}'.format(str(job_id))
 
-print "path, %s, does not exist" %project_path
+# %-formatting
+print("path, %s, does not exist" %project_path)  # Output: path, /tmp/jobs/10, does not exist
+
+
+# str.format()
+print('Hi, my name is {1} & surname is {0}'.format("Patel", "Prakash"))  # Output: Hi, my name is Prakash & surname is Patel
+print('Good {msg}'.format(msg = 'Morning'))  # Output: Good Morning
+
+
+"""
+Python 3's f-Strings
+"""
+name = "Patel Prakash"
+temp_msg = "How are you?"
+
+print(f"Hi my name is {name} & {temp_msg} is my message")  # Output: Hi my name is Patel Prakash & How are you? is my message
+print(f"lower of my name:: {name.lower()}")  # Output: lower of my name:: patel prakash
+
+
+# F-Strings are new and improved way to format strings in Python 3; uses __format__ protocol
+# F-Strings are FASTER than % & .format()
+## Reference:: https://realpython.com/python-f-strings/
